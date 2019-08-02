f555d6b4d4629cea47f70908c7963b68d856e31a
RandyWei <smile561607154@163.com>
2019-08-02 09:04:36 +0800

    add flutter socket listener

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
 import 'package:better_socket/better_socket.dart';
 
 void main() => runApp(MyApp());
@@ -9,14 +12,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _msg = "";
+  String _platformVersion = 'Unknown';
+
   @override
   void initState() {
     super.initState();
-    BetterSocket.connentSocket("ws://123.207.167.163:9010/ajaxchattest")
-        .then((val) {
-      //   print(val);
-      //   BetterSocket.sendMsg("hello");
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await BetterSocket.platformVersion;
+
+      //设置监听
+      BetterSocket.addListener(onOpen: (httpStatus, httpStatusMessage) {
+        print("onOpen---httpStatus:$httpStatus  httpStatusMessage:$httpStatusMessage");
+      }, onMessage: (message) {
+        print("onMessage---message:$message");
+      }, onClose: (code, reason, remote) {
+        print("onClose---code:$code  reason:$reason  remote:$remote");
+      }, onError: (message) {
+        print("onError---message:$message");
+      });
+
+      BetterSocket.connentSocket("ws://123.207.167.163:9010/ajaxchattest").then((val) {
+        print(val);
+        //BetterSocket.sendMsg("hello");
+      });
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
     });
   }
 
@@ -27,28 +63,10 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('发送消息'),
-              onPressed: () {
-                BetterSocket.sendMsg("hello").then((msg) {
-                  setState(() {
-                    _msg = msg;
-                  });
-                });
-              },
-            ),
-            Text(_msg)
-          ],
+        body: Center(
+          child: Text('Running on: $_platformVersion\n'),
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    BetterSocket.close();
-    super.dispose();
   }
 }
